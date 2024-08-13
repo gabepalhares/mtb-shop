be33855ebb6af8bbb6f24ab6b09a35eccf79fbdb
minortypo <gabrielpalharesdev@gmail.com>
2024-08-13 15:52:59 -0300

    refactor: removes padding properties conflicting with keen slider

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -3,7 +3,6 @@ import { transform } from "next/dist/build/swc";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
@@ -13,10 +12,10 @@ export const HomeContainer = styled('main', {
 export const Product = styled('a', {
   background: '$offwhite',
   borderRadius: 8,
-  padding: '0.25rem',
   cursor: 'pointer',
   position: 'relative',
   overflow: 'hidden',
+  minWidth: 540,
 
   display: 'flex',
   alignItems: 'center',
