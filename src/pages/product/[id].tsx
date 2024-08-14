ecab1838a65be293da9eeda595009ffb2ce957db
minortypo <gabrielpalharesdev@gmail.com>
2024-08-14 15:38:57 -0300

    feat: creates product page initial structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product'
 import { useRouter } from 'next/router'
 
 export default function Product() {
   const {query} = useRouter()
   
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Bike</h1>
+        <span>preco 00,00</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam tempore aspernatur pariatur suscipit. Repudiandae molestias culpa excepturi nostrum quaerat dolorem sit quo modi itaque rem, delectus eum commodi id eos!</p>
+
+        <button>comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
