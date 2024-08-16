d4bdf4e713d3de76c21b66d5c6ef795594220a02
minortypo <gabrielpalharesdev@gmail.com>
2024-08-16 17:10:05 -0300

    feat: creates first implementation of success page with static properties

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Congrats <strong>Gabriel</strong>! Your brand new bike <strong>Stumpjumper</strong>is already descending to your home.
+      </p>
+
+      <Link href="/">
+        Back to catalog
+      </Link>
+    </SuccessContainer>
   );
 }
