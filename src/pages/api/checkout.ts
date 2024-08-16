5c1ec0d4724b71ccc3cc16ecd2d53d6125491b6d
minortypo <gabrielpalharesdev@gmail.com>
2024-08-16 16:31:29 -0300

    feat: creates functions to prevent user to access other http method, creates http status messages and json responses

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { stripe } from "@/lib/stripe";
+import { error } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  const priceID = 'price_1PnMA3CQNFkEDe4C9cZvv184';
+export default async function handler(req: NextApiRequest, res: NextApiResponse,) {
+  const { priceId } = req.body;
   
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed.'})
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.'})
+  }
+
   const successUrl = `${process.env.NEXT_URL}/success`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
@@ -17,7 +23,7 @@ export default async function handler(
     mode: 'payment',
     line_items: [
       {
-        price: priceID,
+        price: priceId,
         quantity: 1,
       }
     ],
